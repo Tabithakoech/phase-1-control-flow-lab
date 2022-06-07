b2b65f6cab38e7c23839e416dbf0df26d996e8d4
TabithaKoech <tabbyk1718@gmail.com>
2022-06-07 13:26:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+    // Write your code here!
+    if (ride <= 400) {
+        return "This one is on me!";
+
+    }
+    if (ride > 2500) {
+        return "No can do.";
+
+    } else if (ride >= 2000) {
+        return "I will gladly take your thirty bucks.";
+
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+    // Write your code here!
+    if (city === 'NYC') {
+        return "Ok, sounds good."
+    } 
+    else {
+        return "No go."
+    }
+
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+    // Write your code here!
+    if (tip==='generous'){
+        return "Thank you so much."
+    }
+    
+    else if(tip==="not as generous"){
+        return "Thank you."
+    }
+    else{
+        return "Bye."
+    }
+    
 }
